7c13dcad493be2dad8c53cecf465f971bd98cfac
Sasky828373 <martinmeny940@gmail.com>
2026-09-22 10:34:54 +0200

    Add RAGE native draw bridge ABI

--- native-renderer/src/native_dispatch.h
+++ native-renderer/src/native_dispatch.h
@@ -4,6 +4,8 @@
 extern "C" {
 enum GtavNativeResourceKind : uint32_t { GTAV_NATIVE_BUFFER=1, GTAV_NATIVE_IMAGE_VIEW=2, GTAV_NATIVE_SAMPLER=3 };
 struct GtavNativeResourceHandle { uint64_t rage_handle; uint64_t vk_handle; uint32_t kind; uint32_t generation; };
+struct GtavNativeDrawState { VkCommandBuffer command_buffer; uint32_t flags; };
+using GtavNativeGetDrawState = bool(*)(void*,GtavNativeDrawState*);
 struct GtavNativeDispatch {
  uint32_t abi_version;
  bool (*ready)();
@@ -18,6 +20,10 @@ struct GtavNativeDispatch {
  void (*draw)(VkCommandBuffer,uint32_t,uint32_t);
  void (*draw_indexed)(VkCommandBuffer,uint32_t,uint32_t,int32_t);
  void (*dispatch)(VkCommandBuffer,uint32_t,uint32_t,uint32_t);
+ void (*set_draw_state_provider)(GtavNativeGetDrawState);
+ bool (*rage_draw)(void*,uint32_t,uint32_t);
+ bool (*rage_draw_indexed)(void*,uint32_t,uint32_t,int32_t);
+ bool (*rage_dispatch)(void*,uint32_t,uint32_t,uint32_t);
 };
 __attribute__((visibility("default"))) const GtavNativeDispatch* gtav_native_renderer_get_dispatch();
 }
